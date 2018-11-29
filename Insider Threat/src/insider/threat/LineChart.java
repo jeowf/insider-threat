5525b6b68f2ce82ce7c93f92aa9918e3d0193d20
Felipe Morais <moraisfelipe97@gmail.com>
2018-11-29 19:36:09 -0300

    Plotando histograma

--- Insider Threat/src/insider/threat/LineChart.java
+++ Insider Threat/src/insider/threat/LineChart.java
@@ -12,6 +12,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
@@ -20,13 +21,13 @@ import org.jfree.data.category.DefaultCategoryDataset;
  */
 public class LineChart extends JFrame{
     
-    public LineChart(String title, String chartTitle) {
+    public LineChart(String title, String chartTitle, double[] histogram, String user) {
         super(title);
         
         JFreeChart lineChart = ChartFactory.createLineChart(
          chartTitle,
          "Hours of day","Frequency",
-         createDataset(),
+         createDataset(histogram, user),
          PlotOrientation.VERTICAL,
          true,true,false);
          
@@ -35,15 +36,28 @@ public class LineChart extends JFrame{
       setContentPane( chartPanel );
     }
     
-    private DefaultCategoryDataset createDataset( ) {
+    private DefaultCategoryDataset createDataset(double [] histogram, String user ) {
       DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+      
+        for (int i = 0; i < 24; i++) {
+            dataset.addValue(histogram[i], user, new Integer(i));
+        }
+      /*
       dataset.addValue( 15 , "schools" , "1970" );
+      
+      
       dataset.addValue( 30 , "schools" , "1980" );
       dataset.addValue( 60 , "schools" ,  "1990" );
       dataset.addValue( 120 , "schools" , "2000" );
       dataset.addValue( 240 , "schools" , "2010" );
-      dataset.addValue( 300 , "schools" , "2014" );
+      dataset.addValue( 300 , "schools" , "2014" );*/
       return dataset;
    }
+    
+    public void plot(){
+        pack();
+        RefineryUtilities.centerFrameOnScreen(this);
+        setVisible(true);
+    }
     
 }
--- Insider Threat/src/insider/threat/Main.java
+++ Insider Threat/src/insider/threat/Main.java
@@ -8,6 +8,7 @@ package insider.threat;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jfree.ui.RefineryUtilities;
@@ -17,41 +18,56 @@ import org.jfree.ui.RefineryUtilities;
  * @author morai
  */
 public class Main {
-    public static void main(String[] args) {
-        
-        LineChart chart = new LineChart(
-         "School Vs Years" ,
-         "Numer of Schools vs years");
 
-      chart.pack( );
-      RefineryUtilities.centerFrameOnScreen( chart );
-      chart.setVisible( true );
+    public static void main(String[] args) {
+
         
+
         // Lendo arquivo de preferencias
         String preferencesPath = "prefs.txt";
-        
+
         IOManager ioManager;
         DataManager dataManager = new DataManager();
-        
+
         LocalDate beginDate = null;//LocalDate.of(2010, 1, 4);
         LocalDate endDate = null; //LocalDate.of(2010, 12, 5);
-        
-        if (args.length == 1)
+
+        if (args.length == 1) {
             preferencesPath = args[0];
-        
+        }
+
         try {
             ioManager = new IOManager(preferencesPath);
             ioManager.read(dataManager, beginDate, endDate);
-            ioManager.writeOutput( dataManager.generateAnalyze("Foreman", beginDate, endDate));
+            ioManager.writeOutput(dataManager.generateAnalyze("Foreman", beginDate, endDate));
         } catch (IOException ex) {
             System.out.println("Problems to open the necessary files. Please, check the README.md!");
             System.exit(0);
         }
+
+        Scanner scan = new Scanner(System.in);
+        
+        String s = "5";
+        String id = "";
+        System.out.println("Insira os IDs. ");
+        while(!s.equals(id)){
+            id = scan.next();
+            User u = dataManager.getHashMap().get(id);
+            double[] histogram = dataManager.normalize(u.getHistogram());
+            
+            LineChart chart = new LineChart(
+                "User Histogram (" + u.getName() + ")",     
+                u.getName(),
+                histogram,
+                u.getName()
+            );
+            
+            chart.plot();
+        }
         
         System.out.println("TESTE");
-
         
-        
+        System.exit(0);
     }
-    
+
 }
