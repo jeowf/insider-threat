19978111919d4dce203e5d0b0dca7f52649f5104
Felipe Morais <moraisfelipe97@gmail.com>
2018-11-28 22:34:33 -0300

    Leitura de log

--- Insider Threat/src/insider/threat/DataManager.java
+++ Insider Threat/src/insider/threat/DataManager.java
@@ -6,6 +6,8 @@
 package insider.threat;
 
 import insider.threat.LogEntry.LogType;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 /**
@@ -45,8 +47,68 @@ public class DataManager {
 
             User user = new User(id, name, domain, email, role);
             insertUser(user);
-            int a = 1;
+
+        } else if (log.getLogType() == LogType.LOGON) {
+            String id = fields[0];
+            String date = fields[1];
+            String user = fields[2];
+            String pc = fields[3].split("-")[1];
+            String value = fields[4];
+            String[] splitDate = date.split(" ");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            LocalDate dateTime = LocalDate.parse(splitDate[0], formatter);
+            Date d = new Date(dateTime);
+
+            User searchUser = hashmap.get(user.split("/")[1]);
+
+            if (searchUser != null) {
+                PC newPc = new PC(pc);
+                LogonActivity activity = new LogonActivity(id, value);
+                searchUser.insert(d, newPc, activity, splitDate[1]);
+            }
+        } else if (log.getLogType() == LogType.DEVICE) {
+            String id = fields[0];
+            String date = fields[1];
+            String user = fields[2];
+            String pc = fields[3].split("-")[1];
+            String value = fields[4];
+            String[] splitDate = date.split(" ");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            LocalDate dateTime = LocalDate.parse(splitDate[0], formatter);
+            Date d = new Date(dateTime);
+
+            User searchUser = hashmap.get(user.split("/")[1]);
+
+            if (searchUser != null) {
+                PC newPc = new PC(pc);
+                DeviceActivity activity = new DeviceActivity(id, value);
+                searchUser.insert(d, newPc, activity, splitDate[1]);
+            }
+
+        } else if (log.getLogType() == LogType.HTTP) {
+            String id = fields[0];
+            String date = fields[1];
+            String user = fields[2];
+            String pc = fields[3].split("-")[1];
+            String value = fields[4];
+            String[] splitDate = date.split(" ");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            LocalDate dateTime = LocalDate.parse(splitDate[0], formatter);
+            Date d = new Date(dateTime);
+
+            User searchUser = hashmap.get(user.split("/")[1]);
+            
+            if (searchUser != null) {
+                PC newPc = new PC(pc);
+                HTTPActivity activity = new HTTPActivity(id, value);
+                searchUser.insert(d, newPc, activity, splitDate[1]);
+            }
         }
+        
+        int debug_step = 0;
 
     }
 
